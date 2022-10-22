372f1de4007a3ed09dbd744087d2a50339e6a8c4
Matt Styles <matt@personalurban.com>
2022-10-22 11:48:56 +0100

    update export sketch-react-loop application type

--- packages/sketch-react-loop/src/index.tsx
+++ packages/sketch-react-loop/src/index.tsx
@@ -2,6 +2,8 @@ import type {Application} from 'sketch-loop'
 import {useEffect, useRef} from 'react'
 import {loop} from 'sketch-loop'
 
+export type {Application} from 'sketch-loop'
+
 export type Props = {
   onTick: (opts: {app: Application; dt: number}) => void
 }
